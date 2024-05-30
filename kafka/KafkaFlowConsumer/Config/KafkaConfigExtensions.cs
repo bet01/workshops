6b7c4a67146a0b1b6dcc402c904017180248bc91
Nicholas Cook <nicholas.cook@betsoftware.com>
2024-05-30 05:18:33 +0000

    chore: comments

--- kafka/KafkaFlowConsumer/Config/KafkaConfigExtensions.cs
+++ kafka/KafkaFlowConsumer/Config/KafkaConfigExtensions.cs
@@ -16,13 +16,19 @@ public static class KafkaConfigExtensions
             .AddConsumer(consumer => consumer
                 .Topic(kafkaConfig.Weather.Topic)
                 .WithGroupId(kafkaConfig.ConsumerGroup)
+                // New consumers will start at the beginning
                 .WithAutoOffsetReset(AutoOffsetReset.Earliest)
+                // Message must be manually marked as complete for the offsets to be committed
                 .WithManualMessageCompletion()
+                // How many messages to buffer
                 .WithBufferSize(kafkaConfig.BatchMessageLimit)
                 .WithWorkersCount(10)
                 .AddMiddlewares(middlewares => middlewares
+                    // All messages in this topic are assumed to be of the same type
                     .AddSingleTypeDeserializer<WeatherMessage, JsonCoreDeserializer>()
+                    // Batch messages for batch processing
                     .AddBatching(kafkaConfig.BatchMessageLimit, TimeSpan.FromMilliseconds(kafkaConfig.BatchTimeLimit))
+                    // Batch message handler
                     .Add<WeatherMessageBatchConsumer>()
                 )
             )
