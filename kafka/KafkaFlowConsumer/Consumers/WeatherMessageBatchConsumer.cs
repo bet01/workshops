82d19ed888f8e32f796ad9288dac85430f37b56c
Nicholas Cook <nicholas.cook@betsoftware.com>
2024-05-31 08:34:02 +0000

    feat: never move on until messages completed

--- kafka/KafkaFlowConsumer/Consumers/WeatherMessageBatchConsumer.cs
+++ kafka/KafkaFlowConsumer/Consumers/WeatherMessageBatchConsumer.cs
@@ -5,22 +5,40 @@ namespace KafkaFlowConsumer;
 
 public class WeatherMessageBatchConsumer(ILogger<WeatherMessageBatchConsumer> logger) : IMessageMiddleware
 {
-    ILogger<WeatherMessageBatchConsumer> _logger = logger;
-
     public Task Invoke(IMessageContext context, MiddlewareDelegate next)
     {
         var batch = context.GetMessagesBatch();
         string batchInfo = LogHelper.MessageBatchInfo(batch);
 
-        _logger.LogInformation("Batch received - {batchInfo}", batchInfo);
+        logger.LogInformation("Batch received - {batchInfo}", batchInfo);
 
         foreach (var message in batch)
         {
-            _logger.LogDebug("Message - {info}, workerId: {workerId}", LogHelper.MessageDetails(message), message.ConsumerContext.WorkerId);
+            bool success = false;
+
+            /* With this loop we never want to move on until a message is complete. 
+            This will ensure messages are not missed. This is OK to handle infrastrucutre issues, such as DB connections, timeouts, etc.
+            However it can add load if no back-off retry strategy is in place (check out Polly). You may also end up having to manually tell Kafka to skip 
+            messages if there is a legitimate message issue, like a bad messaged, etc. */
+            do
+            {
+                try
+                {
+                    logger.LogDebug("Message - {info}, workerId: {workerId}", LogHelper.MessageDetails(message), message.ConsumerContext.WorkerId);
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Exception occured while processing message - {batchInfo} offset: {offset}",
+                    batchInfo,
+                    message.ConsumerContext.Offset);
+                }
+            } while (!success);
+
             message.ConsumerContext.Complete();
         }
 
-        _logger.LogInformation("Batch completed - {batchInfo}", batchInfo);
+        logger.LogInformation("Batch completed - {batchInfo}", batchInfo);
 
         return Task.CompletedTask;
     }
